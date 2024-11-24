42bc2b8d281d49ba63df8bdb1d9dc62ec4e5ca97
faycaltaghelit <faycal.taghelit@yutopia.io>
2024-11-24 16:45:55 +0100

    Add files via upload

--- map.py
+++ map.py
@@ -6,7 +6,7 @@ db_connection = mysql.connector.connect(
     host="localhost",
     user="root",
     password="",
-    database="belib_db"  # Your database name
+    database="belib_db"  
 )
 
 cursor = db_connection.cursor()
@@ -19,13 +19,13 @@ LEFT JOIN stationsCara c ON s.station_id = c.station_id
 """
 cursor.execute(query)
 
-# Fetch all rows
+ 
 stations = cursor.fetchall()
 
-# Initialize a Folium map centered on Paris
+ 
 m = folium.Map(location=[48.8566, 2.3522], zoom_start=12)
 
-# Process the station data and add them to the map
+ 
 for station in stations:
     nom, adresse, latitude, longitude, statut, horaires, puissance = station
 
@@ -38,17 +38,17 @@ for station in stations:
         Puissance: {puissance if puissance else 'Inconnue'} kW
     """
 
-    # Add a marker for each station
+    
     folium.Marker(
         location=[latitude, longitude],
         popup=popup_content
     ).add_to(m)
 
-# Save the map to an HTML file
-file_path = r"C:\Users\Hp Omen\Desktop\map.html"  # Update this path as needed
+ 
+file_path = r"C:\Users\Hp Omen\Desktop\map.html"  
 m.save(file_path)
 
-# Close the database connection
+ 
 cursor.close()
 db_connection.close()
 
